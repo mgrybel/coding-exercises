ffad8bf811dcf9590f4901fc0c3c71a50c11bd7e
mgrybel <grybel.m.m@gmail.com>
2024-07-18 02:43:59 +0200

    Update

---
+++ java/strings/ConvertToLowercase.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ConvertToLowercase {
+    static Scanner cin = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String a, b;
+
+        System.out.println("Enter a sentence with uppercase letters:");
+        a = cin.nextLine();
+
+        b = a.toLowerCase();
+
+        System.out.println("The original sentence: " + a);
+        System.out.println("The sentence converted to lowercase: " + b);
+
+    }
+}
