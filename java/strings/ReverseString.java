6886fa041237753aa9cdf90208c37ce8f588e97c
mgrybel <grybel.m.m@gmail.com>
2024-07-20 05:06:14 +0200

    Update

--- java/strings/ReverseString.java
+++ java/strings/ReverseString.java
@@ -6,13 +6,15 @@ public class ReverseString {
     static Scanner cin = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String a;
+        String a, b;
 
         System.out.println("Enter a string to reverse: ");
         a = cin.nextLine();
 
+        b = new StringBuilder(a).reverse().toString();
+
         System.out.println("The original string: " + a);
-        System.out.println("The reversed string: " + new StringBuilder(a).reverse());
+        System.out.println("The reversed string: " + b);
 
     }
 }
