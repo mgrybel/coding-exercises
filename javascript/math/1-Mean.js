19741dc3744f324faeaf3781edc3a020167ba685
mgrybel <grybel.m.m@gmail.com>
2024-07-21 11:42:15 +0200

    Update

---
+++ javascript/math/1-Mean.js
@@ -0,0 +1,15 @@
+// Write a program that calculates the arithmetic mean
+// of three numbers entered by the user.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const firstNumber = parseFloat(prompt('Enter the first number: '));
+const secondNumber = parseFloat(prompt('Enter the second number: '));
+const thirdNumber = parseFloat(prompt('Enter the third number: '));
+
+// Calculate the arithmetic mean
+const mean = (firstNumber + secondNumber + thirdNumber) / 3;
+
+console.log(
+  `The arithmetic mean of ${firstNumber}, ${secondNumber}, and ${thirdNumber} is ${mean}.`
+);
