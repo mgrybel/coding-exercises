2fd5177cc0e0ae739abb769687ff1b00756c7d43
mgrybel <grybel.m.m@gmail.com>
2024-07-21 11:49:43 +0200

    Update

--- java/strings/CompareStrings.java
+++ java/strings/CompareStrings.java
@@ -6,14 +6,14 @@ public class CompareStrings {
     static Scanner cin = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String a, b;
+        String firstStr, secondStr;
 
         System.out.println("Enter the first string to compare: ");
-        a = cin.nextLine();
+        firstStr = cin.nextLine();
 
         System.out.println("Enter the second string to compare");
-        b = cin.nextLine();
+        secondStr = cin.nextLine();
 
-        System.out.println("Is string 1 equal to string 2? " + a.equals(b));
+        System.out.println("Is string 1 equal to string 2? " + firstStr.equals(secondStr));
     }
 }
