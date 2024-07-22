5bdaddb797de720178d5557502a6c35e5656f730
mgrybel <grybel.m.m@gmail.com>
2024-07-22 06:55:11 +0200

    Update

---
+++ javascript/math/3-SquareRoot.js
@@ -0,0 +1,11 @@
+// Write a program that reads a number
+// and prints the square root of the number on the screen.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const userInput = parseFloat(prompt('Enter a number: '));
+
+// Calculate the square root regardless of whether the input is valid
+const squareRoot = Math.sqrt(userInput);
+
+console.log(`The square root of ${userInput} is equal to ${squareRoot}`);
