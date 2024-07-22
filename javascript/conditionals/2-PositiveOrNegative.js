71b0b692055c064c7808e28a69e753e15881e26f
mgrybel <grybel.m.m@gmail.com>
2024-07-22 07:14:38 +0200

    Update

---
+++ javascript/conditionals/2-PositiveOrNegative.js
@@ -0,0 +1,15 @@
+// Write a program that reads a number
+// and reports whether it is positive, negative, or zero.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const num = parseFloat(prompt('Enter a number: '));
+
+// Check if the number is positive, negative, or zero
+if (num > 0) {
+  console.log(`${num} is a positive number.`);
+} else if (num < 0) {
+  console.log(`${num} is a negative number.`);
+} else {
+  console.log(`${num} is zero.`);
+}
