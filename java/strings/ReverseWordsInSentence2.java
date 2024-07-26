8bccfdc0e14d897a61ece069cc2a89ac7dec3100
mgrybel <grybel.m.m@gmail.com>
2024-07-26 08:58:55 +0200

    Update

--- java/strings/ReverseWordsInSentence2.java
+++ java/strings/ReverseWordsInSentence2.java
@@ -6,23 +6,23 @@ public class ReverseWordsInSentence2 {
     static Scanner cin = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String a, b = "";
+        String originalStr, reversedStr = "";
         String[] words;
 
         System.out.println("Enter a sentence to reverse: ");
-        a = cin.nextLine();
+        originalStr = cin.nextLine();
 
-        words = a.split(" ");
+        words = originalStr.split(" ");
 
         for (int i = words.length - 1; i >= 0; i--) {
-            b += words[i];
+            reversedStr += words[i];
 
             if (i != 0) {
-                b += " ";
+                reversedStr += " ";
             }
         }
 
-        System.out.println("The original sentence: " + a);
-        System.out.println("The reversed sentence: " + b);
+        System.out.println("The original sentence: " + originalStr);
+        System.out.println("The reversed sentence: " + reversedStr);
     }
 }
