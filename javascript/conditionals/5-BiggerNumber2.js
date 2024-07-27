a4d65ce15166fd3960b4ab2665a47dec8e31d25f
mgrybel <grybel.m.m@gmail.com>
2024-07-27 22:04:17 +0200

    Update

---
+++ javascript/conditionals/5-BiggerNumber2.js
@@ -0,0 +1,12 @@
+// Write a program that reads two numbers
+// and tells you which one is bigger
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const num1 = parseFloat(prompt('Enter the first number: '));
+const num2 = parseFloat(prompt('Enter the second number: '));
+
+// Find the bigger number using the Marh.max function
+const biggerNum = Math.max(num1, num2);
+
+console.log(`${biggerNum} is the bigger number.`);
