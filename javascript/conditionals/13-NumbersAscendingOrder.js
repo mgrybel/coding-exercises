90779b5c113b7a3c9884db6f276ced19f121d4df
mgrybel <grybel.m.m@gmail.com>
2024-07-27 22:36:45 +0200

    Update

---
+++ javascript/conditionals/13-NumbersAscendingOrder.js
@@ -0,0 +1,46 @@
+// Write a program that reads three numbers
+// and displays then in ascending order.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const num1 = parseFloat(prompt('Enter the first number: '));
+const num2 = parseFloat(prompt('Enter the second number: '));
+const num3 = parseFloat(prompt('Enter the third number: '));
+
+let smallest, middle, largest;
+
+if (num1 <= num2 && num1 <= num3) {
+  smallest = num1;
+
+  if (num2 <= num2) {
+    middle = num2;
+    largest = num3;
+  } else {
+    middle = num3;
+    largest = num2;
+  }
+} else if (num2 <= num1 && num2 <= num3) {
+  smallest = num2;
+
+  if (num1 <= num3) {
+    middle = num1;
+    largest = num3;
+  } else {
+    middle = num3;
+    largest = num1;
+  }
+} else {
+  smallest = num3;
+
+  if (num1 <= num2) {
+    middle = num1;
+    largest = num2;
+  } else {
+    middle = num2;
+    largest = num1;
+  }
+}
+
+console.log(
+  `The numbers in ascending order: ${smallest}, ${middle}, ${largest}.`
+);
