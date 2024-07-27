d696a778f08a2c5d4f4222d8cabcf228b5243580
mgrybel <grybel.m.m@gmail.com>
2024-07-27 22:53:00 +0200

    Update

---
+++ javascript/conditionals/14-Triangle.js
@@ -0,0 +1,18 @@
+// Write a program that reads three numbers
+// and tells you if they can be the sides of a triangle
+// (the sum of two sides must always be greater than the third side).
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const side1 = parseFloat(prompt('Enter the length of side 1: '));
+const side2 = parseFloat(prompt('Enter the length of side 2: '));
+const side3 = parseFloat(prompt('Enter the length of side 3: '));
+
+const canFormTriangle =
+  side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2;
+
+if (canFormTriangle) {
+  console.log('These numbers can form the sides of a triangle.');
+} else {
+  console.log('These numbers cannot form the sides of a triangle.');
+}
