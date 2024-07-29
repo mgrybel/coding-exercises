a0ac05bb2230fe85c44ff54115028d4ac0b1f76b
mgrybel <grybel.m.m@gmail.com>
2024-07-30 00:33:09 +0200

    Update

--- javascript/basics/1-PrintName.js
+++ javascript/basics/1-PrintName.js
@@ -1,4 +1,4 @@
-// Write a program that asks for the user's name
+// Write a program that asks for a user's name
 // and prints the name on the screen immediately after.
 
 const prompt = require('prompt-sync')({ sigint: true });
