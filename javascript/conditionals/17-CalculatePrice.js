ad04aa544bad3b9780a53d3c44c39ecfe27f59a5
mgrybel <grybel.m.m@gmail.com>
2024-07-29 11:35:24 +0200

    Update

---
+++ javascript/conditionals/17-CalculatePrice.js
@@ -0,0 +1,22 @@
+// Write a program that reads the price of a product.
+// If the price is more than 200 USD, the tax is 5%.
+// If it is more than 500 USD, the tax is 7.5%.
+// Products up to 200 USD do not pay taxes.
+// Print the final price of the product on the screen.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const price = parseFloat(prompt('Enter the price of the product: '));
+
+let finalPrice;
+
+// Calculate the final price based on different tax rates
+if (price > 500) {
+  finalPrice = price * 1.075; // 7.5% tax
+} else if (price > 200) {
+  finalPrice = price * 1.05; // 5% tax
+} else {
+  finalPrice = price;
+}
+
+console.log(`The final price of the product is ${finalPrice.toFixed(2)}`);
