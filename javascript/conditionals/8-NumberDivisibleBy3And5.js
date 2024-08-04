079f0c1b0f82b6d18994f280bee4eff85850ee8b
mgrybel <grybel.m.m@gmail.com>
2024-08-04 19:00:16 +0200

    Update

--- javascript/conditionals/8-NumberDivisibleBy3And5.js
+++ javascript/conditionals/8-NumberDivisibleBy3And5.js
@@ -1,9 +1,9 @@
-// Write a program that asks for an integer 
+// Write a program that asks for a number
 // and checks if it is divisible by 3 and 5 at the same time.
 
 const prompt = require('prompt-sync')({ sigint: true });
 
-const num = parseInt(prompt('Enter an integer: '));
+const num = parseFloat(prompt('Enter a number: '));
 
 if (num % 3 === 0 && num % 5 === 0) {
   console.log(`${num} is divisible by both 3 and 5.`);
