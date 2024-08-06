6b1cc69adeced432df2db931bdd32dce9de16f22
mgrybel <grybel.m.m@gmail.com>
2024-08-06 18:52:09 +0200

    Update

---
+++ javascript/loops/8-NumbersBetween.js
@@ -0,0 +1,19 @@
+// Write a program that prompts the user for two numbers A and B
+// and displays all numbers  between A and B.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const A = parseInt(prompt('Enter number A: '));
+const B = parseInt(prompt('Enter number B: '));
+
+console.log(`Numbers between ${A} and ${B}:`);
+
+if (A < B) {
+  for (let i = A + 1; i < B; i++) {
+    console.log(i);
+  }
+} else {
+  for (let i = B + 1; i < A; i++) {
+    console.log(i);
+  }
+}
