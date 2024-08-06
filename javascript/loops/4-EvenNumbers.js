fc20dc1bbd7c6850a41f1a71a2c2603f4119c48b
mgrybel <grybel.m.m@gmail.com>
2024-08-06 18:48:58 +0200

    Update

---
+++ javascript/loops/4-EvenNumbers.js
@@ -0,0 +1,9 @@
+// Write a program that prints all even numbers from 1 to 100
+// using a while loop without using any if statement
+
+let number = 2;
+
+while (number <= 100) {
+  console.log(number);
+  number += 2;
+}
