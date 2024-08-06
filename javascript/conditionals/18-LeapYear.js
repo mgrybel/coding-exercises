2f455ddf2124d0b1f9db1e26e751ec345731a635
mgrybel <grybel.m.m@gmail.com>
2024-08-06 18:46:42 +0200

    Update

---
+++ javascript/conditionals/18-LeapYear.js
@@ -0,0 +1,19 @@
+// Write a program that reads a year (a four digit number)
+// and checks whether it is a leap year or not.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const year = parseInt(prompt('Enter a four-digit year: '));
+
+let isLeapYear = false;
+
+// Check if the year is a leap year
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  isLeapYear = true;
+}
+
+if (isLeapYear) {
+  console.log(`${year} is a leap year.`);
+} else {
+  console.log(`${year} is not a leap year.`);
+}
