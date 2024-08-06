d3fcde43c30dfd9bc1ded1e3413e5f5b4516b2e0
mgrybel <grybel.m.m@gmail.com>
2024-08-06 18:49:28 +0200

    Update

---
+++ javascript/loops/5-EvenAndOdd.js
@@ -0,0 +1,15 @@
+// Write a program that displays even numbers from 1 to 50
+// and odd numbers from 51 to 100
+// using a repeating loop.
+
+for (let number = 1; number <= 100; number++) {
+  if (number <= 50) {
+    if (number % 2 === 0) {
+      console.log(`Even number: ${number}`);
+    }
+  } else {
+    if (number % 2 !== 0) {
+      console.log(`Odd number: ${number}`);
+    }
+  }
+}
