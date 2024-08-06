7e149fc70099c0d4e1601e4f7da7f3e4d65c8d04
mgrybel <grybel.m.m@gmail.com>
2024-08-06 18:53:38 +0200

    Update

---
+++ javascript/loops/11-SumOfEvenNumbers.js
@@ -0,0 +1,13 @@
+// Write a program that calculates and displays
+// the sum of even numbers from 1 to 100
+// using a repeating loop.
+
+let sum = 0;
+let number = 2; // Starting from 2 as the first even number
+
+while (number <= 100) {
+  sum += number; // Add the current even number to the sum
+  number +=2; // Move to the next even number
+}
+
+console.log(`The sum of even numbers from 1 to 100 is: ${sum}`);
