44636e7c2a02ff212c6149ba5cc35e44820588ba
mgrybel <grybel.m.m@gmail.com>
2024-08-12 17:52:12 +0200

    Update

---
+++ javascript/arrays/3-AverageOfElements.js
@@ -0,0 +1,19 @@
+// Write a program that reads an arrays of integers
+// and displays the average of the elements.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+
+const integers = input.split(' ').map(Number);
+
+// Calculate the average of elements in the array
+let sum = 0;
+
+for (let i = 0; i < integers.length; i++) {
+  sum += integers[i];
+}
+
+const average = sum / integers.length;
+
+console.log(`The average of all elements is: ${average}`);
