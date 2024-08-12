0dfcfec0b0eda3c87a7338228452293c1c904e19
mgrybel <grybel.m.m@gmail.com>
2024-08-12 17:49:24 +0200

    Update

---
+++ javascript/loops/15-Divisors.js
@@ -0,0 +1,14 @@
+// Write a program that prompts the user for a number
+// and displays its divisors.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const number = parseInt(prompt('Enter a number: '));
+
+console.log(`The divisors of ${number} are:`);
+
+for (let i = 1; i <= number; i++) {
+  if (number % i === 0) {
+    console.log(i);
+  }
+}
