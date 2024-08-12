6dbe78a09c3f19c9af4b807682492908b1f922fc
mgrybel <grybel.m.m@gmail.com>
2024-08-12 17:51:22 +0200

    Update

---
+++ javascript/arrays/1-NumbersInReverseOrder.js
@@ -0,0 +1,15 @@
+// Write a program that reads an array of integers
+// and displays the elements in reverse order.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+
+const integers = input.split(' ').map(Number);
+
+// Display the elements in reverse order
+console.log(`Elements in reverse order: `);
+
+for (let i = integers.length - 1; i >= 0; i--) {
+  console.log(integers[i]);
+}
