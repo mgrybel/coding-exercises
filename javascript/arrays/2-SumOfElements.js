8245d65ee9d60afc31e94c841c23d032a201e754
mgrybel <grybel.m.m@gmail.com>
2024-08-12 17:51:47 +0200

    Update

---
+++ javascript/arrays/2-SumOfElements.js
@@ -0,0 +1,16 @@
+// Write a program that reads an array of integers
+// and displays the sum of all elements.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+
+const integers = input.split(' ').map(Number);
+
+// Calculate the sum of elements in the array
+let sum = 0;
+for (let i = 0; i < integers.length; i++) {
+  sum += integers[i];
+}
+
+console.log(`The sum of all elements is: ${sum}`);
