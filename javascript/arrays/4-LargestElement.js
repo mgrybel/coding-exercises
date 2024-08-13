b2743f639fcfb7b3c320f1132df0149bada400bd
mgrybel <grybel.m.m@gmail.com>
2024-08-13 11:17:56 +0200

    Update

---
+++ javascript/arrays/4-LargestElement.js
@@ -0,0 +1,18 @@
+// Write a program that reads an array of integers
+// and displays the largest element in the array.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+
+const integers = input.split(' ').map(Number);
+
+// Find the largest element in the array
+let largest = integers[0];
+for (let i = 1; i < integers.length; i++) {
+  if (integers[i] > largest) {
+    largest = integers[i];
+  }
+}
+
+console.log(`The largest element in the array is: ${largest}`);
