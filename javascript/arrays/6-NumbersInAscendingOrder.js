47fab0e88ca7df6a370440f495f96e785e2a9c2f
mgrybel <grybel.m.m@gmail.com>
2024-08-16 03:32:18 +0200

    Update

---
+++ javascript/arrays/6-NumbersInAscendingOrder.js
@@ -0,0 +1,24 @@
+// Write a program that reads an array of integers
+// and checks if they are in ascending order.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+const integers = input.split(' ').map(Number);
+
+// Function to check if the array is in the ascending order
+function isAscending(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Check if the array is in the ascending order and display the result
+if (isAscending(integers)) {
+  console.log('The array is in the ascending order.');
+} else {
+  console.log('The array is not in the ascending order.');
+}
