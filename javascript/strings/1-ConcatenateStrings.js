baa8718d73a961d5ccbd3f6bed05ac9e408b66a4
mgrybel <grybel.m.m@gmail.com>
2024-08-17 00:56:38 +0200

    Update

---
+++ javascript/strings/1-ConcatenateStrings.js
@@ -0,0 +1,11 @@
+// Write a program that reads two words and
+// concatenates them, displaying the resulting word.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const word1 = prompt('Enter the first word: ');
+const word2 = prompt('Enter the second word: ');
+
+const concatenatedWord = word1 + word2;
+
+console.log('The concatenated word:', concatenatedWord);
