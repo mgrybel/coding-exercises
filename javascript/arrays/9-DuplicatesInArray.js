c7614b7cb5631b3ed112b24052a0631ed8d2aa5c
mgrybel <grybel.m.m@gmail.com>
2024-08-16 03:33:58 +0200

    Update

---
+++ javascript/arrays/9-DuplicatesInArray.js
@@ -0,0 +1,28 @@
+// Write a program that checks if an array contains duplicate elements.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const input = prompt('Enter a list of integers separated by spaces: ');
+const integers = input.split(' ').map(Number);
+
+let containsDuplicate = false;
+
+// Check for duplicate elements in the array
+for (let i = 0; i < integers.length; i++) {
+  for (let j = i + 1; j < integers.length; j++) {
+    if (integers[i] === integers[j]) {
+      containsDuplicate = true;
+      break;
+    }
+  }
+  if (containsDuplicate) {
+    break;
+  }
+}
+
+// Display the result
+if (containsDuplicate) {
+  console.log('The array contains duplicate elements.');
+} else {
+  console.log('The array does not contain duplicate elements.');
+}
