04f21dab890fe47ac3b1676d0818feb0409d1be9
mgrybel <grybel.m.m@gmail.com>
2024-08-18 01:26:02 +0200

    Update

---
+++ javascript/strings/4-ReplaceSpaces.js
@@ -0,0 +1,10 @@
+// Write a program that takes a sentence and
+// replaces all spaces with a new line.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const sentence = prompt('Enter a sentence: ');
+let replacedSentence = sentence.split(' ').join('\n');
+
+console.log('Modified sentence:');
+console.log(replacedSentence);
