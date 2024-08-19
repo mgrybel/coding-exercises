4ed8c26a38a24cec1a7d5020010276b23ad68cb1
mgrybel <grybel.m.m@gmail.com>
2024-08-19 15:49:12 +0200

    Update

---
+++ javascript/strings/5-CheckNameStartsWith.js
@@ -0,0 +1,12 @@
+// Write a program that receives a name and
+// checks if it starts with the letter 'A'.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const name = prompt('Enter a name: ');
+
+if (name.charAt(0).toUpperCase() === 'A') {
+  console.log(`${name} starts with the letter A.`);
+} else {
+  console.log(`${name} does not start with the letter A.`);
+}
