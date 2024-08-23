ce258d044e28e99c9e87e45a514893458da96742
mgrybel <grybel.m.m@gmail.com>
2024-08-24 00:47:47 +0200

    Update

---
+++ javascript/strings/10-CountVowels.js
@@ -0,0 +1,16 @@
+// Write a program that reads a word and displays
+// the number of vowels present in it.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const word = prompt('Enter a word: ');
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let vowelCount = 0;
+
+for (let i = 0; i < word.length; i++) {
+  if (vowels.includes(word.charAt(i).toLowerCase())) {
+    vowelCount++;
+  }
+}
+
+console.log('The number of vowels:', vowelCount);
