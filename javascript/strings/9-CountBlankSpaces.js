1591f67e4c1782c3acaf1acc586170d67c882d6f
mgrybel <grybel.m.m@gmail.com>
2024-08-23 05:59:58 +0200

    Update

---
+++ javascript/strings/9-CountBlankSpaces.js
@@ -0,0 +1,15 @@
+// Write a program that receives a sentence and
+// displays the amount of blank spaces present in it.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const sentence = prompt('Enter a sentence: ');
+let spaceCount = 0;
+
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence.charAt(i) === ' ') {
+    spaceCount++;
+  }
+}
+
+console.log('The number of blank spaces:', spaceCount);
