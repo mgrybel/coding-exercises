8453cb9919415f295ad30ae9d056d67e914a1370
mgrybel <grybel.m.m@gmail.com>
2024-08-25 15:07:21 +0200

    Update

---
+++ javascript/strings/11-CountVowelsAndConsonants.js
@@ -0,0 +1,22 @@
+// Write a program that reads a word and displays
+// the number of vowels and consonants present in it.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const word = prompt('Enter a word: ');
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let vowelCount = 0;
+let consonantCount = 0;
+
+for (let i = 0; i < word.length; i++) {
+  const currentChar = word.charAt(i).toLowerCase();
+
+  if (vowels.includes(currentChar)) {
+    vowelCount++;
+  } else if (currentChar >= 'a' && currentChar <= 'z') {
+    consonantCount++;
+  }
+}
+
+console.log('The number of vowels:', vowelCount);
+console.log('The number of consonants:', consonantCount);
