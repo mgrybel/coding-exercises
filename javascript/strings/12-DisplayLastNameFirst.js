45deecff184c1ce85e7d1aecf238294be446978f
mgrybel <grybel.m.m@gmail.com>
2024-08-26 02:56:45 +0200

    Update

---
+++ javascript/strings/12-DisplayLastNameFirst.js
@@ -0,0 +1,11 @@
+// Write a program that takes a full name and
+// displays the last name first.
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const fullName = prompt('Enter your full name: ');
+const namesArray = fullName.split(' ');
+const lastName = namesArray[namesArray.length - 1];
+const firstName = namesArray[0];
+
+console.log(`${lastName} ${firstName}`);
