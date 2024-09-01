07855a5ae71de2377b66005c68e5d73ce7a793c1
mgrybel <grybel.m.m@gmail.com>
2024-09-01 22:26:22 +0200

    Update

---
+++ javascript/matrices/14-Minesweeper.js
@@ -0,0 +1,5 @@
+// Write a program that reads an m x n matrix, indicating the location
+// where there are mines in a Minesweeper game (being 0 for a neutral field,
+// and 1 for locations where there would be mines), and the program should
+// return a matrix indicating, for each position, the number of mines
+// in neighboring houses.
