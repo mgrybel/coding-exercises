4691d76ed1339d808a4833113eef929c7e57db86
mgrybel <grybel.m.m@gmail.com>
2024-09-01 22:25:48 +0200

    Update

---
+++ javascript/matrices/12-Multiplication.js
@@ -0,0 +1,3 @@
+// Write a program that reads two matrices and returns the multiplication 
+// between them as an answer. The program should observe whether or not it is
+// possible to perform the multiplication between the two matrices.
