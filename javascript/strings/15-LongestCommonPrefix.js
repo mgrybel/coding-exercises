19664cd96e15141a4beb3d7d04ed40e7166e4b28
mgrybel <grybel.m.m@gmail.com>
2024-09-01 10:12:38 +0200

    Update

---
+++ javascript/strings/15-LongestCommonPrefix.js
@@ -0,0 +1,30 @@
+// Given a list of strings, write a function
+// to determine the longest common prefix.
+
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) {
+    return '';
+  }
+
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.slice(0, j);
+    if (prefix === '') {
+      return '';
+    }
+  }
+  return prefix;
+}
+
+// Example usage
+const strings = ['flower', 'flow', 'flight'];
+const commonPrefix = longestCommonPrefix(strings);
+console.log('The longest common prefix:', commonPrefix);
