34982494e03f747a4a874e165d75dd14809a2062
mgrybel <grybel.m.m@gmail.com>
2024-09-04 19:09:01 +0200

    Update

--- javascript/matrices/4-NegativeNumbersInMatrix.js
+++ javascript/matrices/4-NegativeNumbersInMatrix.js
@@ -1,1 +1,21 @@
 // Count negative numbers in a matrix.
+
+const matrix = [
+  [2, -3, 5, 0],
+  [-7, 8, -1, 4],
+  [6, 0, -9, 10],
+  [-12, -15, 17, 20],
+];
+
+const negativeCount = 0;
+
+// Count negative numbers in the matrix
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j] < 0) {
+      negativeCount++;
+    }
+  }
+}
+
+console.log('The number of negative numbers in the matrix:', negativeCount);
