a588add26739ba32a5c0e41e543f67aa09169c92
mgrybel <grybel.m.m@gmail.com>
2024-09-06 00:23:13 +0200

    Update

--- javascript/matrices/6-SumOfBoundaryElements.js
+++ javascript/matrices/6-SumOfBoundaryElements.js
@@ -1,1 +1,26 @@
 // Calculate the sum of boundary elements in a matrix.
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let sum = 0;
+
+// Calculate sum of boundary elements
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (
+      i === 0 ||
+      i === matrix.length - 1 ||
+      j === 0 ||
+      j === matrix[i].length - 1
+    ) {
+      sum += matrix[i][j];
+    }
+  }
+}
+
+console.log('Matrix:', matrix);
+console.log('The sum of boundary elements:', sum);
