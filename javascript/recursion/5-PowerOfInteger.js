c4fa61638d2f7d244669c86f58f6ce527b14d044
mgrybel <grybel.m.m@gmail.com>
2024-09-07 01:33:34 +0200

    Update

---
+++ javascript/recursion/5-PowerOfInteger.js
@@ -0,0 +1,15 @@
+// Write a recursive function to calculate
+// the power of an integer raised to an exponent.
+
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+
+// Example usage
+const base = 3;
+const exp = 4;
+console.log(`${base} raised to the power of ${exp} is:`, power(base, exp)); // Expected output: 81
