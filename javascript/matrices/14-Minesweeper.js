dc51e24fdbd330a0a1510de63df5e714d92e0c44
mgrybel <grybel.m.m@gmail.com>
2024-09-07 01:30:37 +0200

    Update

--- javascript/matrices/14-Minesweeper.js
+++ javascript/matrices/14-Minesweeper.js
@@ -3,3 +3,40 @@
 // and 1 for locations where there would be mines), and the program should
 // return a matrix indicating, for each position, the number of mines
 // in neighboring houses.
+
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const result = [];
+
+  for (let i = 0; i < rows; i++) {
+    result.push([]);
+    for (let j = 0; j < cols; j++) {
+      let count = 0;
+      for (let r = i - 1; r <= i + 1; r++) {
+        for (let c = j - 1; c <= j + 1; c++) {
+          if (
+            r >= 0 &&
+            r < rows &&
+            c >= 0 &&
+            c < cols &&
+            !(r === i && c === j)
+          ) {
+            count += matrix[r][c];
+          }
+        }
+      }
+      result[i][j] = count;
+    }
+  }
+  return result;
+}
+
+// Example matris with mine locations
+const matrix = [
+  [0, 1, 0],
+  [1, 1, 0],
+  [0, 1, 0],
+];
+
+console.log(minesweeper(matrix));
