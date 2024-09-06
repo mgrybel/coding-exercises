c7217a4dcd8a7e70e6e3a6a8ccf73b587feb66bd
mgrybel <grybel.m.m@gmail.com>
2024-09-07 01:33:12 +0200

    Update

---
+++ javascript/recursion/4-SumOfDigitsOfInteger.js
@@ -0,0 +1,13 @@
+// Write a recursive function to calculate the sum of the digits of an integer
+
+function sumOfDigits(n) {
+  if (n < 10) {
+    return n;
+  } else {
+    return (n % 10) + sumOfDigits(Math.floor(n / 10));
+  }
+}
+
+// Example usage
+const number = 12345;
+console.log('The sum of digits:', sumOfDigits(number)); // Expected output: 15
