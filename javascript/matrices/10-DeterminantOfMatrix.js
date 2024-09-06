89e47d4dd28daa4128267ce1b6bc7aa8d7d2b8b9
mgrybel <grybel.m.m@gmail.com>
2024-09-07 01:29:04 +0200

    Update

--- javascript/matrices/10-DeterminantOfMatrix.js
+++ javascript/matrices/10-DeterminantOfMatrix.js
@@ -1,1 +1,16 @@
 // Write a program that reads a 3x3 matrix and calculates the determinant of the matrix.
+
+function determinant(matrix) {
+  const [[a, b, c], [d, e, f], [g, h, i]] = matrix;
+  return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+}
+
+// Example matrix
+const matrix = [
+  [2, -3, 1],
+  [5, 4, 7],
+  [0, 8, -6],
+];
+
+console.log('Matrix:', matrix);
+console.log('Determinant:', determinant(matrix));
