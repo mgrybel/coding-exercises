958b0a156b705c0f5ec37ae854884207515fbe27
mgrybel <grybel.m.m@gmail.com>
2024-09-07 01:32:51 +0200

    Update

--- javascript/recursion/3-PrimeNumber.js
+++ javascript/recursion/3-PrimeNumber.js
@@ -1,1 +1,21 @@
 // Create a recursive function to check if a number is prime.
+
+function isPrime(num, divisor = 2) {
+  if (num <= 2) {
+    return n === 2;
+  }
+
+  if (num % divisor === 0) {
+    return num === divisor;
+  }
+
+  if (divisor * divisor > num) {
+    return true;
+  }
+
+  return isPrime(num, divisor + 1);
+}
+
+// Example usage
+const number = 17;
+console.log(`Is ${number} prime?`, isPrime(number)); // Expected output: true
