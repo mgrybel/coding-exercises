3e3dc3891b714c2e07176a4ecf91ab5781858dc3
mgrybel <grybel.m.m@gmail.com>
2024-09-10 17:53:18 +0200

    Update

---
+++ javascript/recursion/10-SumOfArrayElements.js
@@ -0,0 +1,13 @@
+// Write a recursive function to calculate the sum of elements of an array.
+
+function arraySum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr[0] + arraySum(arr.slice(1));
+  }
+}
+
+// Example usage
+const array = [1, 2, 3, 4, 5];
+console.log('The sum of elements in the array:', arraySum(array)); // Expected output: 15
