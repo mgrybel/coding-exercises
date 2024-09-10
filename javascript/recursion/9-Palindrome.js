cf6bede1aa643fd44c8c1d2aea21f4b9cb667978
mgrybel <grybel.m.m@gmail.com>
2024-09-10 17:52:08 +0200

    Update

---
+++ javascript/recursion/9-Palindrome.js
@@ -0,0 +1,21 @@
+// Write a recursive function to determine whether a word is a palindrome.
+// Palindrome is a word, phrase, or sequence that reads the same backwards as forwards,
+// e.g. 'madam' or 'nurses run'.
+
+function isPalindrome(word) {
+  if (word.length <= 1) {
+    return true;
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      return isPalindrome(word.substring(1, word.length - 1));
+    } else {
+      return false;
+    }
+  }
+}
+
+// Example usage
+const word1 = 'racecar';
+const word2 = 'hello';
+console.log(`${word1} is a palindrome:`, isPalindrome(word1)); // Expected output: true
+console.log(`${word2} is a palindrome:`, isPalindrome(word2)); // Expected output: false
