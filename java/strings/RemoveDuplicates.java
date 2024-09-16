f3749fc284ed72961274b8a598b952bb32379788
mgrybel <grybel.m.m@gmail.com>
2024-09-16 23:25:45 +0200

    Update

---
+++ java/strings/RemoveDuplicates.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+// Remove duplicate characters from a given string
+// Input: hackerearth
+// Output: hackert
+public class RemoveDuplicates {
+    static Scanner cin = new Scanner(System.in);
+
+    public static String removeDup(String inputString) {
+        String lowercaseString = inputString.toLowerCase();
+        char[] characters = lowercaseString.toCharArray();
+        LinkedHashSet<Character> uniqueCharacters = new LinkedHashSet<>();
+
+        for (char character : characters) {
+            if (!uniqueCharacters.contains(character)) {
+                uniqueCharacters.add(character);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (char character : uniqueCharacters) {
+            result.append(character);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String inputString = "hackerearth";
+        String result = removeDup(inputString);
+        System.out.println(result);
+    }
+  
+}
