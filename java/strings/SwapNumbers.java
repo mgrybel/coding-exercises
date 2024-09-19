a175367e55c3a4b7f9d6961da8a8eabd554e0f66
mgrybel <grybel.m.m@gmail.com>
2024-09-19 11:21:46 +0200

    Update

---
+++ java/strings/SwapNumbers.java
@@ -0,0 +1,19 @@
+package strings;
+
+// Write a program to swap two numbers
+// using the substring method.
+// Input: 10 20
+// Output: 20 10
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 20;
+
+        firstNumber = firstNumber + secondNumber;
+        secondNumber = firstNumber - secondNumber;
+        firstNumber = firstNumber - secondNumber;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+    }
+}
