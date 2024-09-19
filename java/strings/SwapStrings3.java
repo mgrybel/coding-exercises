9b3afea32ca138434a301be380e65e4c6091a33e
mgrybel <grybel.m.m@gmail.com>
2024-09-19 11:16:42 +0200

    Update

---
+++ java/strings/SwapStrings3.java
@@ -0,0 +1,21 @@
+package strings;
+
+// Write a program to swap two strings
+// using the StringBuilder class.
+// Input: Hello World
+// Output: World Hello
+public class SwapStrings3 {
+    public static void main(String[] args) {
+        String firstString = "Hello";
+        String secondString = "World";
+
+        StringBuilder sb = new StringBuilder(firstString);
+        sb.append(secondString);
+
+        secondString = sb.substring(0, firstString.length());
+        firstString = sb.substring(secondString.length());
+
+        System.out.println(firstString);
+        System.out.println(secondString);
+    }
+}
