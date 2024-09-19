b3a39fe812f9b5821849a67d04c060a09083b057
mgrybel <grybel.m.m@gmail.com>
2024-09-19 11:11:26 +0200

    Update

---
+++ java/strings/SwapStrings2.java
@@ -0,0 +1,19 @@
+package strings;
+
+// Write a program to swap two strings using a temp variable.
+// Input: Hello World
+// Output: World Hello
+public class SwapStrings2 {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = "World";
+        String temp;
+
+        temp = string1;
+        string1 = string2;
+        string2 = temp;
+
+        System.out.println(string1);
+        System.out.println(string2);
+    }
+}
