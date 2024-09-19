c48028bb620b3f33c8f42c2060ce408864babbf6
mgrybel <grybel.m.m@gmail.com>
2024-09-19 11:07:47 +0200

    Update

---
+++ java/strings/SwapStrings.java
@@ -0,0 +1,19 @@
+package strings;
+
+// Write a program to swap two strings
+// using a substring method.
+// Input: Hello world
+// Output: World Hello
+public class SwapStrings {
+    public static void main(String[] main) {
+        String string1 = "Hello";
+        String string2  = "World";
+
+        string1 = string1 + string2;
+        string2 = string1.substring(0, string1.length() - string2.length());
+        string1 = string1.substring(string2.length());
+
+        System.out.println(string1);
+        System.out.println(string2);
+    }
+}
