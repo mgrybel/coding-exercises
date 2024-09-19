1ecd18888bda1ab2dbcec4f2dfa4c67b904f8256
mgrybel <grybel.m.m@gmail.com>
2024-09-19 11:29:55 +0200

    Update

---
+++ java/strings/PrimeNumbers.java
@@ -0,0 +1,28 @@
+package strings;
+
+// Write a program to check whether
+// the given number is prime or not.
+// Input: 7
+// Output: prime
+public class PrimeNumbers {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        if (isPrime(17)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+}
