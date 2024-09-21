a7dbbe11c8415a131f759c756eeebc14409c08dc
mgrybel <grybel.m.m@gmail.com>
2024-09-21 11:03:14 +0200

    Update

---
+++ python/math/1-Mean.py
@@ -0,0 +1,12 @@
+# Write a program that calculates the arithmetic mean
+# of two numbers.
+
+# Prompt the user for two numbers
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+
+# Calculate the arithmetic mean
+mean = (num1 + num2) / 2
+
+# Display the result
+print('Arithmetic mean:', mean)
