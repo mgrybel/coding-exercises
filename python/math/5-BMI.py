f785de368f7fd65a273706736db8c959321bcd4c
mgrybel <grybel.m.m@gmail.com>
2024-09-27 12:47:25 +0200

    Update

---
+++ python/math/5-BMI.py
@@ -0,0 +1,12 @@
+# Write a program that calculates the BMI of an individual
+# using the formula BMI = weight / height^2
+
+# Prompr the user for weight and height
+weight = float(input('Enter your weight in kilograms: '))
+height = float(input('Enter your height in meters: '))
+
+# Calculate the BMI
+bmi = weight / (height ** 2)
+
+# Display the result
+print('Your BMI is:', bmi)
