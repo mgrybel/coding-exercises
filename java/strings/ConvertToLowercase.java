04e5520cca3a9492bb01f9f55cc3d6ebba264478
mgrybel <grybel.m.m@gmail.com>
2024-10-07 21:26:10 +0200

    Update

--- java/strings/ConvertToLowercase.java
+++ java/strings/ConvertToLowercase.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class ConvertToLowercase {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter a sentence with uppercase letters:");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         secondStr = firstStr.toLowerCase();
 
         System.out.println("The original sentence: " + firstStr);
         System.out.println("The sentence converted to lowercase: " + secondStr);
+
+        scanner.close(); 
     }
 }
