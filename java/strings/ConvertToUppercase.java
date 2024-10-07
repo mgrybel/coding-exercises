4907528c0f17b450629d097d307d2720564e421f
mgrybel <grybel.m.m@gmail.com>
2024-10-07 21:26:50 +0200

    Update

--- java/strings/ConvertToUppercase.java
+++ java/strings/ConvertToUppercase.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class ConvertToUppercase {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter a sentence with lowercase letters:");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         secondStr = firstStr.toUpperCase();
 
         System.out.println("The original sentence: " + firstStr);
         System.out.println("The sentence converted to uppercase: " + secondStr);
+
+        scanner.close();
     }
 }
