15ed70c5a506438d846263e2832d6d48afaa8b16
mgrybel <grybel.m.m@gmail.com>
2024-10-07 21:25:45 +0200

    Update

--- java/strings/CompareStrings.java
+++ java/strings/CompareStrings.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class CompareStrings {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter the first string to compare: ");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         System.out.println("Enter the second string to compare");
-        secondStr = cin.nextLine();
+        secondStr = scanner.nextLine();
 
         System.out.println("Is string 1 equal to string 2? " + firstStr.equals(secondStr));
+
+        scanner.close();
     }
 }
