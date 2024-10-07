cf9e0da829aa8ded9a144bfc9b327e03e12c722b
mgrybel <grybel.m.m@gmail.com>
2024-10-07 21:30:28 +0200

    Update

--- java/strings/FindStringPosition.java
+++ java/strings/FindStringPosition.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class FindStringPosition {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] main) {
+        Scanner scanner = new Scanner(System.in);
+
         String myStr;
         int position;
 
         System.out.println("Enter a sentence with the word Java in it: ");
-        myStr = cin.nextLine();
+        myStr = scanner.nextLine();
 
         position = myStr.indexOf("Java");
 
         System.out.println("The position is: " + position);
+
+        scanner.close();
     }
 }
