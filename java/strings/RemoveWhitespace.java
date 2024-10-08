c8ba974bf6bb5f74996883d48f2a60d9c30e30d2
mgrybel <grybel.m.m@gmail.com>
2024-10-08 05:21:10 +0200

    Update

--- java/strings/RemoveWhitespace.java
+++ java/strings/RemoveWhitespace.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class RemoveWhitespace {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter a string with whitespace characters: ");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         secondStr = firstStr.trim();
 
         System.out.println("The original string: " + firstStr);
         System.out.println("The string without whitespace characters: " + secondStr);
+
+        scanner.close();
     }
 }
