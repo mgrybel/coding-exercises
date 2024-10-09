6250e7fd01b7d841ae1437782d5dff1ee2487fa3
mgrybel <grybel.m.m@gmail.com>
2024-10-09 05:42:32 +0200

    Update

--- java/strings/ReplacePartOfString.java
+++ java/strings/ReplacePartOfString.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class ReplacePartOfString {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter a sentence with the word C++ in it: ");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         secondStr = firstStr.replace("C++", "Java");
 
         System.out.println("The original sentence: " + firstStr);
         System.out.println("The modified sentence: " + secondStr);
+
+        scanner.close();
     }
 }
