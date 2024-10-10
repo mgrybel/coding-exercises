ee0e37702910baba637218cdeb8d6ad8dfa50d25
mgrybel <grybel.m.m@gmail.com>
2024-10-10 18:40:59 +0200

    Update

--- java/strings/ReverseString.java
+++ java/strings/ReverseString.java
@@ -3,17 +3,19 @@ package strings;
 import java.util.Scanner;
 
 public class ReverseString {
-    static Scanner cin = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String firstStr, secondStr;
 
         System.out.println("Enter a string to reverse: ");
-        firstStr = cin.nextLine();
+        firstStr = scanner.nextLine();
 
         secondStr = new StringBuilder(firstStr).reverse().toString();
 
         System.out.println("The original string: " + firstStr);
         System.out.println("The reversed string: " + secondStr);
+
+        scanner.close();
     }
 }
