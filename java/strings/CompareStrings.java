5ddf972e85bc071aedc4c5719ed53115a89fd32d
mgrybel <grybel.m.m@gmail.com>
2024-10-11 07:36:10 +0200

    Update

--- java/strings/CompareStrings.java
+++ java/strings/CompareStrings.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CompareStrings {
     public static void main(String[] args) {
+        // Create a scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
 
         String firstStr, secondStr;
@@ -16,6 +17,7 @@ public class CompareStrings {
 
         System.out.println("Is string 1 equal to string 2? " + firstStr.equals(secondStr));
 
+        // Close the scanner
         scanner.close();
     }
 }
