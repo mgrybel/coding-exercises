fd6ee7c007ff4d8789af2b9e51e52e0cc6e7c295
mgrybel <grybel.m.m@gmail.com>
2024-10-11 07:38:44 +0200

    Update

--- java/strings/GetPartOfString.java
+++ java/strings/GetPartOfString.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GetPartOfString {
     public static void main(String[] args) {
+        // Create a scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
 
         String myStr;
@@ -14,6 +15,7 @@ public class GetPartOfString {
         System.out.println("The substring from index 4 till the end is: " + myStr.substring(4));
         System.out.println("The substring from index 4 till index 9 is: " + myStr.substring(4, 9));
 
+        // Close the scanner
         scanner.close();
     }
 }
