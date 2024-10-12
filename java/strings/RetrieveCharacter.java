273ae043441304ff25e49dd8a53da74bfabf35c0
mgrybel <grybel.m.m@gmail.com>
2024-10-12 22:15:45 +0200

    Update

--- java/strings/RetrieveCharacter.java
+++ java/strings/RetrieveCharacter.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class RetrieveCharacter {
     public static void main(String[] args) {
+        // Create a scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
 
         String myStr;
@@ -14,6 +15,7 @@ public class RetrieveCharacter {
         System.out.println("The character at index 0: " + myStr.charAt(0));
         System.out.println("The character at index 9: " + myStr.charAt(9));
 
+        // Close the scanner
         scanner.close();
     }
 }
