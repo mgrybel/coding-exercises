b2e5124602874ec5252bf740f830ebbc2388a2d2
mgrybel <grybel.m.m@gmail.com>
2024-10-13 15:30:25 +0200

    Update

---
+++ python/conditionals/7-Score.py
@@ -0,0 +1,3 @@
+# Write a program that reads the scores of two tests and determines
+# whether the student passed (score greater than or equal to 6) or
+# failed (score less than 6) in each of the tests
