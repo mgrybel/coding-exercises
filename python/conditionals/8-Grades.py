bbce24f7a2bd99e60f77ff096c024dfe38169bac
mgrybel <grybel.m.m@gmail.com>
2024-10-13 15:30:57 +0200

    Update

---
+++ python/conditionals/8-Grades.py
@@ -0,0 +1,4 @@
+# Write a program that reads the grades of two tests, calculates
+# the simple arithmetic mean, and determines whether the student
+# passed (average greater than or equal to 6) or failed
+# (average less than 6).
