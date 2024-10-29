4b5ba1dc29f5117aaa307b90f56a822bbebd3287
mgrybel <grybel.m.m@gmail.com>
2024-10-29 05:58:58 +0100

    Update

--- python/conditionals/5-PositiveNegativeOrZero.py
+++ python/conditionals/5-PositiveNegativeOrZero.py
@@ -1,2 +1,13 @@
 # Write a program that reads a number and determines
 # whether it is positive, negative or zero.
+
+# Prompt the user to enter a number
+number = float(input('Enter a number: '))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+  print('The number is positive.')
+elif number < 0:
+  print('The number is negative.')
+else:
+  print('The number is zero.')
