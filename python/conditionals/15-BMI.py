ca81169a55758ae9c4a47bd2771ccc967b3e0bea
mgrybel <grybel.m.m@gmail.com>
2024-10-30 15:12:45 +0100

    Update

---
+++ python/conditionals/15-BMI.py
@@ -0,0 +1,3 @@
+# Write a program that asks for a person's height and weight and calculates their BMI 
+# (body mass index), displaying the corresponding category (underweight, normal weight,
+# overweight, obese, severely obese).
