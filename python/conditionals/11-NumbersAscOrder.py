ce1dc09e82256d1a57a624c05328f87846ea0a08
mgrybel <grybel.m.m@gmail.com>
2024-11-05 19:11:36 +0100

    Update

--- python/conditionals/11-NumbersAscOrder.py
+++ python/conditionals/11-NumbersAscOrder.py
@@ -1,2 +1,24 @@
 # Write a program that reads three numbers and displays
 # them on the screen in ascending order.
+
+# Prompt the user to enter three numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+
+# Check the order of the numbers using if statements
+if number1 <= number2 and number1 <= number3:
+  if number2 <= number3:
+    print("The numbers in ascending order are:", number1, number2, number3)
+  else:
+    print("The numbers in ascending order are:", number1, number3, number2)
+elif number2 <= number1 and number2 <= number3:
+  if number1 <= number3:
+    print("The numbers in ascending order are:", number2, number1, number3)
+  else:
+    print("The numbers in ascending order are:", number2, number3, number1)
+else:
+  if number1 <= number2:
+    print("The numbers in ascending order are:", number3, number1, number2)
+  else:
+    print("The numbers in ascending order are:", number3, number2, number1)
