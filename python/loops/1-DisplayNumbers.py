f9fa8a670467dc89cea0d4185cd398cc73b0d2cb
mgrybel <grybel.m.m@gmail.com>
2024-11-08 06:52:11 +0100

    Update

--- python/loops/1-DisplayNumbers.py
+++ python/loops/1-DisplayNumbers.py
@@ -1,1 +1,9 @@
-# Write a program that displays the numbers 1 through 10 using a loop.
+# Write a program that displays the numbers 1 through 10 using a while loop.
+
+# Initialize a variable to start at 1
+num = 1
+
+# Use a while loop to iterate until the number reaches 11
+while num <= 10:
+  print(num)
+  num += 1
