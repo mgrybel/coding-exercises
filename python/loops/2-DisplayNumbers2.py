2cb970b62b0f9f128b99ede81abb4820cc2770ee
mgrybel <grybel.m.m@gmail.com>
2024-11-17 14:07:30 +0100

    Update

--- python/loops/2-DisplayNumbers2.py
+++ python/loops/2-DisplayNumbers2.py
@@ -1,5 +1,5 @@
 # Write a program that displays the numbers 1 through 10 using a for loop.
 
 # Use a for loop to iterate through the numbers 1 to 10
-for num in range(1, 11):
-  print(num)
+for number in range(1, 11):
+  print(number)
