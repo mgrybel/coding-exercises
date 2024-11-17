f011fb162c95e360f85ca9fb282a098ea96b4eda
mgrybel <grybel.m.m@gmail.com>
2024-11-17 14:08:43 +0100

    Update

--- python/loops/5-DisplayEvenNumbers2.py
+++ python/loops/5-DisplayEvenNumbers2.py
@@ -1,9 +1,9 @@
 # Write a program that prints all even numbers from 1 to 100.
 
 # Initialize a variable to start at 2
-num = 2
+number = 2
 
 # Use a while loop to iterate until the number reaches 100
-while num <= 100:
-  print(num)
-  num += 2
+while number <= 100:
+  print(number)
+  number += 2
