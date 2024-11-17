a3433fd66b57f1c07fadfa3cf44631c3253422e9
mgrybel <grybel.m.m@gmail.com>
2024-11-17 14:10:21 +0100

    Update

--- python/loops/7-DisplayEvenAndOdd.py
+++ python/loops/7-DisplayEvenAndOdd.py
@@ -2,14 +2,14 @@
 # odd numbers 51 to 100 using a loop.
 
 # Initialize a variable to start at 1
-num = 1
+number = 1
 
 # Use a while loop to iterate until the number reaches 100
-while num <= 100:
-  if num <= 50:
-    if num % 2 == 0:
-      print("Even:", num)
+while number <= 100:
+  if number <= 50:
+    if number % 2 == 0:
+      print("Even:", number)
   else:
-    if num % 2 != 0:
-      print("Odd:", num)
-  num += 1
+    if number % 2 != 0:
+      print("Odd:", number)
+  number += 1
