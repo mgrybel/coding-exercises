ebd5c423e9db6ab801954cd0269d59b06bf4407b
mgrybel <grybel.m.m@gmail.com>
2024-11-17 13:52:53 +0100

    Update

---
+++ python/loops/19-NumbersUntilNegative2.py
@@ -0,0 +1,17 @@
+# Write a program that reads numbers from the user until a negative number
+# is entered, and prints the sum of the positive numbers.
+
+# Initialize variables
+sum_positive = 0
+number = 0
+
+# Read numbers from the user until a negative number is entered
+while number >= 0:
+  number = int(input("Enter a number (negative number to exit:): "))
+
+  # Check if the number is positive
+  if number >= 0:
+    sum_positive += number
+
+# Print the sum of positive numbers
+print("Sum of positive numbers:", sum_positive)
