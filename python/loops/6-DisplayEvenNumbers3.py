c2fc7d65c30efd604da88e3152053d4d9016d9e0
mgrybel <grybel.m.m@gmail.com>
2024-11-17 14:09:02 +0100

    Update

--- python/loops/6-DisplayEvenNumbers3.py
+++ python/loops/6-DisplayEvenNumbers3.py
@@ -1,5 +1,5 @@
 # Write a program that prints all even numbers from 1 to 100.
 
 # Use a for loop to iterate through the even numbers from 2 to 100
-for num in range(2, 101, 2):
-  print(num)
+for number in range(2, 101, 2):
+  print(number)
