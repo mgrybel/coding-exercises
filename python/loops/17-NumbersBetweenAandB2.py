2e734107b537c0d0948596e839b63cc9e7b543f9
mgrybel <grybel.m.m@gmail.com>
2024-11-17 12:22:13 +0100

    Update

---
+++ python/loops/17-NumbersBetweenAandB2.py
@@ -0,0 +1,22 @@
+# Write a program that prompts the user for two numbers A and B
+# and displays all numbers between A and B.
+
+# Prompt the user for the two numbers
+A = int(input("Enter the value of A: "))
+B = int(input("Enter the value of B: "))
+
+# Determine the starting and ending values for the loop
+if A <= B:
+  start = A
+  end = B
+else:
+  start = B
+  end = A
+
+# Use a for loop to iterate over the numbers
+# between A and B (inclusive)
+for num in range(start, end + 1):
+  print(num, end=" ")
+
+# Add a new line for better output formatting
+print()
