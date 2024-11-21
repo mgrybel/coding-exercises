fd6d1207b9e4fa3ae86eadb8f3923997e4e4026e
mgrybel <grybel.m.m@gmail.com>
2024-11-21 19:07:00 +0100

    Update

--- python/loops/11-SumOfEvenNumbers2.py
+++ python/loops/11-SumOfEvenNumbers2.py
@@ -5,9 +5,9 @@
 sum_of_evens = 0
 
 # Use a for loop with range and step 2 to iterate through even numbers from 2 to 100
-for num in range(2, 101, 2):
+for number in range(2, 101, 2):
   # Add the current number to the sum
-  sum_of_evens += num
+  sum_of_evens += number
 
 # Display the sum of even numbers
 print("The sum of even numbers from 1 to 100 is:", sum_of_evens)
