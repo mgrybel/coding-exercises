d68e2b59f202c34e860b99b95e781fc445224f7a
mgrybel <grybel.m.m@gmail.com>
2024-11-27 18:33:09 +0100

    Update

--- java/strings/ReverseString2/ReverseString2.java
+++ java/strings/ReverseString2/ReverseString2.java
@@ -2,6 +2,9 @@ package strings.ReverseString2;
 
 import java.util.Scanner;
 
+// Reverse a given string
+// Input: This is a string
+// Output: gnirts a si sihT
 public class ReverseString2 {
     public static void main(String[] args) {
         // Create a scanner object to read input from the user
