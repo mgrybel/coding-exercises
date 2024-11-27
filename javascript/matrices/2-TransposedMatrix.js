691413bed525e19a168f66248a312f786f6130d0
mgrybel <grybel.m.m@gmail.com>
2024-11-27 18:35:23 +0100

    Update

--- javascript/matrices/2-TransposedMatrix.js
+++ javascript/matrices/2-TransposedMatrix.js
@@ -1,5 +1,4 @@
-// Write a program that fills a 4x4 matrix
-// with random values and displays the transposed matrix.
+// Write a program that fills a 4x4 matrix with random values and displays the transposed matrix.
 
 const matrix = [];
 const transposedMatrix = [];
