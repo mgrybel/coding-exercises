ede5ba15c26bedee64b66c836f428f90e4c62b6d
mgrybel <grybel.m.m@gmail.com>
2024-11-28 22:06:10 +0100

    Update

--- python/math/9-PerimeterAreaOfRectangle.py
+++ python/math/9-PerimeterAreaOfRectangle.py
@@ -1,5 +1,5 @@
 # Write a program that calculates the perimeter and area of a rectangle,
-# using the formula P = 2(w + 1) and A = wl where w is the width and l is the length
+# using the formula P = 2(w + 1) and A = wl where w is the width and l is the length.
 
 # Prompt the user for the width and length of the rectangle
 width = float(input('Enter the width of the rectangle: '))
