7fd644055593dcb6aabdd7eb64841c3905d6daf2
mgrybel <grybel.m.m@gmail.com>
2024-12-22 11:30:57 +0100

    Update

--- javascript/conditionals/1-EvenOrOdd.js
+++ javascript/conditionals/1-EvenOrOdd.js
@@ -2,11 +2,11 @@
 
 const prompt = require('prompt-sync')({ sigint: true });
 
-const num = parseFloat(prompt('Enter a number: '));
+const number = parseFloat(prompt('Enter a number: '));
 
 // Check if the number is even or odd
-if (num % 2 === 0) {
-  console.log(`${num} is an even number.`);
+if (number % 2 === 0) {
+  console.log(`${number} is an even number.`);
 } else {
-  console.log(`${num} is an odd number.`);
+  console.log(`${number} is an odd number.`);
 }
