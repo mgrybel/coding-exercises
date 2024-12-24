d893862ee136291a121c6630896e35a914efa985
mgrybel <grybel.m.m@gmail.com>
2024-12-24 22:09:25 +0100

    Update

--- javascript/conditionals/5-BiggerNumber2.js
+++ javascript/conditionals/5-BiggerNumber2.js
@@ -6,7 +6,7 @@ const number1 = parseFloat(prompt('Enter the first number: '));
 const number2 = parseFloat(prompt('Enter the second number: '));
 
 // Find the bigger number using the Marh.max function
-const biggerNum = Math.max(number1, number2);
+const biggerNumber = Math.max(number1, number2);
 
 // Display the result
-console.log(`${biggerNum} is the bigger number.`);
+console.log(`${biggerNumber} is the bigger number.`);
