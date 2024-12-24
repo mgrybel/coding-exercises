81459e4a79ba7a3ce3dd22cf94a6de25d683cd2c
mgrybel <grybel.m.m@gmail.com>
2024-12-24 22:09:53 +0100

    Update

--- javascript/loops/6-PrimeOrNot.js
+++ javascript/loops/6-PrimeOrNot.js
@@ -1,18 +1,18 @@
-// Write a program that asks the user for a number N
+// Write a program that asks the user for a number
 // and checks whether the number is prime or not.
 
 const prompt = require('prompt-sync')({ sigint: true });
 
-const N = parseInt(prompt('Enter a number: '));
+const number = parseInt(prompt('Enter a number: '));
 
 let isPrime = true;
 
 // Check if the number is prime
-if (N <= 1) {
+if (number <= 1) {
   isPrime = false;
 } else {
-  for (let i = 2; i <= Math.sqrt(N); i++) {
-    if (N % i === 0) {
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
       isPrime = false;
       break;
     }
@@ -20,7 +20,7 @@ if (N <= 1) {
 }
 
 if (isPrime) {
-  console.log(`${N} is a prime number.`);
+  console.log(`${number} is a prime number.`);
 } else {
-  console.log(`${N} is not a prime number`);
+  console.log(`${number} is not a prime number`);
 }
