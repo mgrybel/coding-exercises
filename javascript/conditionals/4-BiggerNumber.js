58a296c771c49618b4cce0cb11b15288080089b4
mgrybel <grybel.m.m@gmail.com>
2024-12-30 21:18:33 +0100

    Update

--- javascript/conditionals/4-BiggerNumber.js
+++ javascript/conditionals/4-BiggerNumber.js
@@ -5,6 +5,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 const number1 = parseFloat(prompt('Enter the first number: '));
 const number2 = parseFloat(prompt('Enter the second number: '));
 
+// Check which of the two numbers is bigger and display the result
 if (number1 > number2) {
   console.log(`${number1} is bigger than ${number2}`);
 } else if (number2 > number1) {
