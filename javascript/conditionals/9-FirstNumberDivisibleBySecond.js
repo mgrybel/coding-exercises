caae15126e2e0554cdc9652ce5e83ae2fe7d1147
mgrybel <grybel.m.m@gmail.com>
2024-12-30 21:21:05 +0100

    Update

--- javascript/conditionals/9-FirstNumberDivisibleBySecond.js
+++ javascript/conditionals/9-FirstNumberDivisibleBySecond.js
@@ -6,6 +6,8 @@ const prompt = require('prompt-sync')({ sigint: true });
 const number1 = parseFloat(prompt('Enter the first number: '));
 const number2 = parseFloat(prompt('Enter the second number: '));
 
+// Check if the first number is divisible by the second
+// and display the result
 if (number1 !== 0 && number1 % number2 === 0) {
   console.log(`${number1} is divisible by ${number2}`);
 } else {
