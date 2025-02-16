90cbc0b1be743ad97a3a41648f3e0af65a4313b3
mgrybel <grybel.m.m@gmail.com>
2025-02-16 18:46:01 +0100

    Update

--- python/arrays/8-SecondLargestElement.py
+++ python/arrays/8-SecondLargestElement.py
@@ -10,7 +10,7 @@ array = []
 # Read the elements of the array from the user
 print("Enter elements for the array:")
 for i in range(n):
-  element = int(input("Enter element {}:".format(i + 1)))
+  element = int(input("Enter element {}: ".format(i + 1)))
   array.append(element)
 
 # Initialize variables to store the largest and second largest elements
