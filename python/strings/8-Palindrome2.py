f6c313fb2257ede2631e171212a5ea396ef30d6f
mgrybel <grybel.m.m@gmail.com>
2025-02-26 19:04:48 +0100

    Update

---
+++ python/strings/8-Palindrome2.py
@@ -0,0 +1,21 @@
+# Write a program that reads a word and checks if 
+# it is a palindrome (if it can be read backwards
+# the same way).
+
+# Case-sensitive
+# Using for loop
+
+word = input("Enter a word: ")
+
+is_palindrome = True
+
+length = len(word)
+for i in range(length // 2):
+  if word[i] != word[length - 1 - i]:
+    is_palindrome = False
+    break
+
+if is_palindrome:
+  print("The word is a palindrome.")
+else:
+  print("The word is not a palindrome.")
