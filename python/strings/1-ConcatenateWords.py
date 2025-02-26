468b562a7ed6afb33aef3426cec2ec83b156d3d1
mgrybel <grybel.m.m@gmail.com>
2025-02-26 18:12:28 +0100

    Update

---
+++ python/strings/1-ConcatenateWords.py
@@ -0,0 +1,9 @@
+# Create a program that reads two words and
+# concatenates them, displaying the result.
+
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+result = word1 + word2
+
+print("Concatenated word: ", result)
