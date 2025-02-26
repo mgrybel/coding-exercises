270a5be357945cd0681030ed76de02b04ecefc9a
mgrybel <grybel.m.m@gmail.com>
2025-02-26 18:39:20 +0100

    Update

---
+++ python/strings/4-ReplaceLetterAwithE.py
@@ -0,0 +1,10 @@
+# Write a program that takes a sentence and
+# replaces all the letters "a" with "e"
+
+# The program replaces only the lowercase "a", not the uppercase "A"
+
+sentence = input("Enter a sentence: ")
+
+new_sentence = sentence.replace("a", "e")
+
+print("The modified sentence:", new_sentence)
