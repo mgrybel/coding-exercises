a7468598a4cc348dff4533db80049453e97184c5
mgrybel <grybel.m.m@gmail.com>
2025-02-26 18:55:25 +0100

    Update

---
+++ python/strings/6-CheckIfNameStartsWithA.py
@@ -0,0 +1,9 @@
+# Write a program that receives a name and checks if
+# it starts with the letter "A".
+
+name = input("Enter a name: ")
+
+if name.startswith("A"):
+  print("The name starts with 'A'.")
+else:
+  print("The name does not start with 'A'.")
