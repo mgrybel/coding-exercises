0d437d0ba50ab17ed93d42eb6e40aa91f7b61e2f
mgrybel <grybel.m.m@gmail.com>
2025-03-02 20:17:44 +0100

    Update

--- javascript/loops/2-ForLoop.js
+++ javascript/loops/2-ForLoop.js
@@ -1,4 +1,5 @@
-// Write a program that displays the numbers from 1 to 10 using a for loop.
+// Write a program that displays the numbers from 1 to 10
+// using a for loop.
 
 for (let number = 1; number <= 10; number++) {
   console.log(number);
