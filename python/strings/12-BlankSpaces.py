4b7e6dc4037ff7174fdb0da7c5ffefa4bc545003
mgrybel <grybel.m.m@gmail.com>
2025-03-02 19:51:04 +0100

    Update

---
+++ python/strings/12-BlankSpaces.py
@@ -0,0 +1,17 @@
+# Write a program that receives a sentence and
+# displays the number of blank spaces present in it.
+
+# Using a for loop
+
+sentence = input("Enter a sentence: ")
+
+# Initialize a variable to count the number of blank spaces
+count = 0
+
+# Iterate over each character in the sentence
+for char in sentence:
+  # Check if the character is a blank space
+  if char == " ":
+    count += 1
+
+print("The number of blank spaces:", count)
