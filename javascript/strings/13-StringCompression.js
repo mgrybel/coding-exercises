d5b4411fd2a0369696e19c18e94a6688ce2ec479
mgrybel <grybel.m.m@gmail.com>
2025-03-08 13:54:50 +0100

    Update

--- javascript/strings/13-StringCompression.js
+++ javascript/strings/13-StringCompression.js
@@ -1,5 +1,4 @@
-// `Implement a method to perform basic string compression
-// using the counts of repeated characters.
+// Implement a method to perform basic string compression using the counts of repeated characters.
 // For example, the string aabcccccaaa would become a2b1c5a3.
 
 const str = 'aabcccccaaa';
