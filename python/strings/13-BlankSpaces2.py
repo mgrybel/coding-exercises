1de713dda44b664622ea21e2eb8c79d75b61b758
mgrybel <grybel.m.m@gmail.com>
2025-04-22 18:56:36 +0200

    Update

---
+++ python/strings/13-BlankSpaces2.py
@@ -0,0 +1,11 @@
+# Write a program that receives a sentence and
+# displays the number of blank spaces present in it.
+
+# Using the count function
+
+sentence = input("Enter a sentence: ")
+
+# Use the count() function to count the number of blank spaces
+count = sentence.count(' ')
+
+print("The number of blank spaces:", count)
