96d72a91c962195d7e39b755bef4181be6380eff
mgrybel <grybel.m.m@gmail.com>
2025-04-25 20:02:55 +0200

    Update

--- python/strings/16-ReversedSentence.py
+++ python/strings/16-ReversedSentence.py
@@ -1,6 +1,7 @@
 # Write a program that prompts the user for a sentence
 # and displays the sentence backwards.
 
+# Prompt the user for a sentence
 sentence = input("Enter a sentence: ")
 
 reversed_sentence = ' '.join(reversed(sentence.split()))
