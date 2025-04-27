df4c24786972834e7a77ea9146c89ab456d9eb82
mgrybel <grybel.m.m@gmail.com>
2025-04-27 23:04:16 +0200

    Update

--- javascript/math/2-Exponentiation.js
+++ javascript/math/2-Exponentiation.js
@@ -1,5 +1,6 @@
-// Write a program that reads two numbers, the first being the base and the second the exponent,
-// and then prints the first number raised to the second.
+// Write a program that reads two numbers, the first being
+// the base and the second the exponent, and then prints
+// the first number raised to the second.
 
 const prompt = require('prompt-sync')({ sigint: true });
 
