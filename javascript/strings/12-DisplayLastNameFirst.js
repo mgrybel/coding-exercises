78ceecd884b81c8ff0aebd4804063e47784d9327
mgrybel <grybel.m.m@gmail.com>
2025-05-11 21:39:46 +0200

    Update

--- javascript/strings/12-DisplayLastNameFirst.js
+++ javascript/strings/12-DisplayLastNameFirst.js
@@ -2,7 +2,9 @@
 
 const prompt = require('prompt-sync')({ sigint: true });
 
+// Prompt the user for a full name
 const fullName = prompt('Enter your full name: ');
+
 const namesArray = fullName.split(' ');
 const lastName = namesArray[namesArray.length - 1];
 const firstName = namesArray[0];
