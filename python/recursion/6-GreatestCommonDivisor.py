62f48aacb0154609df1ad7a3c100f98adeb2c093
mgrybel <grybel.m.m@gmail.com>
2025-07-14 20:41:07 +0200

    Update

---
+++ python/recursion/6-GreatestCommonDivisor.py
@@ -0,0 +1,13 @@
+# Write a recursive function to find the greatest
+# common divisor (GCD) of two numbers.
+
+def gcd(a, b):
+  # Base case: if b is 0, a is GCD
+  if b == 0:
+    return a
+  # Recursive case: compute the GCD of by and
+  # the remainder of a divided by b
+  return gcd(b, a % b)
+
+# Example
+print("The GCD of 12 and 6:", gcd(12, 6))
