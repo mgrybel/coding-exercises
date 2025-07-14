276f0185a88deab84d07473f3f494e309e7e0199
mgrybel <grybel.m.m@gmail.com>
2025-07-14 20:32:50 +0200

    Update

--- python/recursion/4-SumOfDigits.py
+++ python/recursion/4-SumOfDigits.py
@@ -1,2 +1,12 @@
 # Write a recursive function to calculate the sum 
 # of the digits of an integer. 
+
+def sum_of_digits(n):
+  # Base case
+  if n == 0:
+    return 0
+  # Recursive case
+  return (n % 10) + sum_of_digits(n // 10)
+
+# Example
+print("The sum of digits of 55:", sum_of_digits(55))
