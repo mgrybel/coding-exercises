ad9ed8580634260635954933a7e820dcdd7e437c
mgrybel <grybel.m.m@gmail.com>
2025-07-14 20:37:19 +0200

    Update

--- python/recursion/5-PowerOfInteger.py
+++ python/recursion/5-PowerOfInteger.py
@@ -1,2 +1,13 @@
 # Write a recursive function to calculate the power of 
 # an integer raised to an exponent.
+
+def power(base, exponent):
+  # Base case: exponent is 0, return 1
+  if exponent == 0:
+    return 1
+  # Recursive case: multiply base with
+  # power of (base, exponent - 1)
+  return base * power(base, exponent - 1)
+
+# Example
+print("3 to the power of 4:", power(3, 4))
