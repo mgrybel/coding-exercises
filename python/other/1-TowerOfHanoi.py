4504ac13fa91655e5c5a57db4da41ab5ec3aef08
mgrybel <grybel.m.m@gmail.com>
2025-07-19 16:40:44 +0200

    Update

--- python/other/1-TowerOfHanoi.py
+++ python/other/1-TowerOfHanoi.py
@@ -4,10 +4,8 @@ def tower_of_hanoi(n, source, destination, auxiliary):
   if n > 0:
     # Move n - 1 diskis from source to auxiliary rod
     tower_of_hanoi(n - 1, source, auxiliary, destination)
-
     # Move the nth disk from source to destination rod
     print(f"Move disk {n} from {source} to {destination}")
-
     # Move the n - 1 disks from auxiliary to destination rod
     tower_of_hanoi(n - 1, auxiliary, destination, source)
 
